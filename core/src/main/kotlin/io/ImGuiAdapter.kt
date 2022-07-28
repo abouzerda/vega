9d2d6edc02464800cefbc933c20ce2b1f7396e11
abouzerda <amin.bouzerda@udo.edu>
2022-07-29 00:38:32 +0200

    Added ImGui styling. Embrace the dark 🥷.

--- core/src/main/kotlin/io/ImGuiAdapter.kt
+++ core/src/main/kotlin/io/ImGuiAdapter.kt
@@ -3,10 +3,7 @@ package io
 import core.GLFWWindow
 import core.GLFWWindow.height
 import core.GLFWWindow.width
-import imgui.ImFontAtlas
-import imgui.ImFontConfig
-import imgui.ImGui
-import imgui.ImGuiFreeType
+import imgui.*
 import imgui.callback.ImStrConsumer
 import imgui.callback.ImStrSupplier
 import imgui.flag.*
@@ -23,6 +20,7 @@ class ImGuiAdapter(private var glfwWindowHandle: Long) {
     fun init() {
         this.logger.info("ImGui ${ImGui.getVersion()}!")
         ImGui.createContext()
+        setStyles()
         with(ImGui.getIO()) {
             /* We don't want to save .ini file */
             iniFilename = IMGUI_INI_FILE
@@ -69,6 +67,80 @@ class ImGuiAdapter(private var glfwWindowHandle: Long) {
 
         this.logger.info("GLSL ${GLSL_VERSION.removePrefix("#")}!")
         lwjglRenderer.init(GLSL_VERSION)
+    }
+
+    private fun setStyles() {
+        val style = ImGui.getStyle()
+        style.setColor(ImGuiCol.Text, 1.00f, 1.00f, 1.00f, 1.00f)
+        style.setColor(ImGuiCol.TextDisabled, 0.50f, 0.50f, 0.50f, 1.00f)
+        style.setColor(ImGuiCol.WindowBg, 0.13f, 0.14f, 0.15f, 1.00f)
+        style.setColor(ImGuiCol.ChildBg, 0.13f, 0.14f, 0.15f, 1.00f)
+        style.setColor(ImGuiCol.PopupBg, 0.13f, 0.14f, 0.15f, 1.00f)
+        style.setColor(ImGuiCol.Border, 0.43f, 0.43f, 0.50f, 0.50f)
+        style.setColor(ImGuiCol.BorderShadow, 0.00f, 0.00f, 0.00f, 0.00f)
+        style.setColor(ImGuiCol.FrameBg, 0.25f, 0.25f, 0.25f, 1.00f)
+        style.setColor(ImGuiCol.FrameBgHovered, 0.38f, 0.38f, 0.38f, 1.00f)
+        style.setColor(ImGuiCol.FrameBgActive, 0.67f, 0.67f, 0.67f, 0.39f)
+        style.setColor(ImGuiCol.TitleBg, 0.08f, 0.08f, 0.09f, 1.00f)
+        style.setColor(ImGuiCol.TitleBgActive, 0.08f, 0.08f, 0.09f, 1.00f)
+        style.setColor(ImGuiCol.TitleBgCollapsed, 0.00f, 0.00f, 0.00f, 0.51f)
+        style.setColor(ImGuiCol.MenuBarBg, 0.14f, 0.14f, 0.14f, 1.00f)
+        style.setColor(ImGuiCol.ScrollbarBg, 0.02f, 0.02f, 0.02f, 0.53f)
+        style.setColor(ImGuiCol.ScrollbarGrab, 0.31f, 0.31f, 0.31f, 1.00f)
+        style.setColor(ImGuiCol.ScrollbarGrabHovered, 0.41f, 0.41f, 0.41f, 1.00f)
+        style.setColor(ImGuiCol.ScrollbarGrabActive, 0.51f, 0.51f, 0.51f, 1.00f)
+        style.setColor(ImGuiCol.CheckMark, 0.11f, 0.64f, 0.92f, 1.00f)
+        style.setColor(ImGuiCol.SliderGrab, 0.11f, 0.64f, 0.92f, 1.00f)
+        style.setColor(ImGuiCol.SliderGrabActive, 0.08f, 0.50f, 0.72f, 1.00f)
+        style.setColor(ImGuiCol.Button, 0.25f, 0.25f, 0.25f, 1.00f)
+        style.setColor(ImGuiCol.ButtonHovered, 0.38f, 0.38f, 0.38f, 1.00f)
+        style.setColor(ImGuiCol.ButtonActive, 0.67f, 0.67f, 0.67f, 0.39f)
+        style.setColor(ImGuiCol.Header, 0.22f, 0.22f, 0.22f, 1.00f)
+        style.setColor(ImGuiCol.HeaderHovered, 0.25f, 0.25f, 0.25f, 1.00f)
+        style.setColor(ImGuiCol.HeaderActive, 0.67f, 0.67f, 0.67f, 0.39f)
+        style.setColor(ImGuiCol.Separator, ImGuiCol.Border)
+        style.setColor(ImGuiCol.SeparatorHovered, 0.41f, 0.42f, 0.44f, 1.00f)
+        style.setColor(ImGuiCol.SeparatorActive, 0.26f, 0.59f, 0.98f, 0.95f)
+        style.setColor(ImGuiCol.ResizeGrip, 0.00f, 0.00f, 0.00f, 0.00f)
+        style.setColor(ImGuiCol.ResizeGripHovered, 0.29f, 0.30f, 0.31f, 0.67f)
+        style.setColor(ImGuiCol.ResizeGripActive, 0.26f, 0.59f, 0.98f, 0.95f)
+        style.setColor(ImGuiCol.Tab, 0.08f, 0.08f, 0.09f, 0.83f)
+        style.setColor(ImGuiCol.TabHovered, 0.33f, 0.34f, 0.36f, 0.83f)
+        style.setColor(ImGuiCol.TabActive, 0.23f, 0.23f, 0.24f, 1.00f)
+        style.setColor(ImGuiCol.TabUnfocused, 0.08f, 0.08f, 0.09f, 1.00f)
+        style.setColor(ImGuiCol.TabUnfocusedActive, 0.13f, 0.14f, 0.15f, 1.00f)
+        style.setColor(ImGuiCol.DockingPreview, 0.26f, 0.59f, 0.98f, 0.70f)
+        style.setColor(ImGuiCol.DockingEmptyBg, 0.20f, 0.20f, 0.20f, 1.00f)
+        style.setColor(ImGuiCol.PlotLines, 0.61f, 0.61f, 0.61f, 1.00f)
+        style.setColor(ImGuiCol.PlotLinesHovered, 1.00f, 0.43f, 0.35f, 1.00f)
+        style.setColor(ImGuiCol.PlotHistogram, 0.90f, 0.70f, 0.00f, 1.00f)
+        style.setColor(ImGuiCol.PlotHistogramHovered, 1.00f, 0.60f, 0.00f, 1.00f)
+        style.setColor(ImGuiCol.TextSelectedBg, 0.26f, 0.59f, 0.98f, 0.35f)
+        style.setColor(ImGuiCol.DragDropTarget, 0.11f, 0.64f, 0.92f, 1.00f)
+        style.setColor(ImGuiCol.NavHighlight, 0.26f, 0.59f, 0.98f, 1.00f)
+        style.setColor(ImGuiCol.NavWindowingHighlight, 1.00f, 1.00f, 1.00f, 0.70f)
+        style.setColor(ImGuiCol.NavWindowingDimBg, 0.80f, 0.80f, 0.80f, 0.20f)
+        style.setColor(ImGuiCol.ModalWindowDimBg, 0.80f, 0.80f, 0.80f, 0.35f)
+        style.setWindowPadding(8.00f, 8.00f)
+        style.setFramePadding(5.00f, 2.00f)
+        style.setItemSpacing(6.00f, 6.00f)
+        style.setItemInnerSpacing(6.00f, 6.00f)
+        style.setTouchExtraPadding(0.00f, 0.00f)
+        style.indentSpacing = 25f
+        style.scrollbarSize = 15f
+        style.grabMinSize = 10f
+        style.windowBorderSize = 1f
+        style.childBorderSize = 1f
+        style.popupBorderSize = 1f
+        style.frameBorderSize = 1f
+        style.tabBorderSize = 1f
+        style.windowRounding = 7f
+        style.childRounding = 4f
+        style.frameRounding = 10f
+        style.popupRounding = 4f
+        style.scrollbarRounding = 9f
+        style.grabRounding = 3f
+        style.tabRounding = 4f
     }
 
     fun update(dt: Float) {
